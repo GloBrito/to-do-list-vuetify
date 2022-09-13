7bdc7b22c14f327a73d1b8e99670c24d94f929dd
Glória Brito <gloriafbrito@gmail.com>
2022-09-13 14:06:56 -0300

    cria layouts

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,27 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
+    <v-app-bar app color="purple" dark>
       <div class="d-flex align-center">
         <v-img
           alt="Vuetify Logo"
           class="shrink mr-2"
           contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          src="https://cdn-icons-png.flaticon.com/512/4697/4697260.png"
           transition="scale-transition"
-          width="40"
+          width="60"
         />
+        <h1>To do list</h1>
 
-        <v-img
-          alt="Vuetify Name"
+        <v-text
+          text="Vuetify Name"
           class="shrink mt-1 hidden-sm-and-down"
           contain
           min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
           width="100"
         />
       </div>
 
       <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
     </v-app-bar>
 
     <v-main>
